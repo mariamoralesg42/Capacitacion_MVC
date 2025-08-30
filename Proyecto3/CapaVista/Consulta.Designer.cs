0ba07dd9d9368ee98068d627bc9fb38aa3c5762c
María Alejandra Morales García <mmoralesg42@miumg.edu.gt>
2025-08-30 11:48:34 -0600

    Add files via upload

---
+++ Proyecto3/CapaVista/Consulta.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace CapaVista
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Dgv_registros = new System.Windows.Forms.DataGridView();
+            this.Btn_Consultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_registros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Dgv_registros
+            // 
+            this.Dgv_registros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_registros.Location = new System.Drawing.Point(12, 288);
+            this.Dgv_registros.Name = "Dgv_registros";
+            this.Dgv_registros.RowHeadersWidth = 51;
+            this.Dgv_registros.RowTemplate.Height = 24;
+            this.Dgv_registros.Size = new System.Drawing.Size(776, 150);
+            this.Dgv_registros.TabIndex = 0;
+            // 
+            // Btn_Consultar
+            // 
+            this.Btn_Consultar.Location = new System.Drawing.Point(353, 180);
+            this.Btn_Consultar.Name = "Btn_Consultar";
+            this.Btn_Consultar.Size = new System.Drawing.Size(128, 58);
+            this.Btn_Consultar.TabIndex = 1;
+            this.Btn_Consultar.Text = "Consultar";
+            this.Btn_Consultar.UseVisualStyleBackColor = true;
+            this.Btn_Consultar.Click += new System.EventHandler(this.Btn_Consultar_Click);
+            // 
+            // Consulta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Consultar);
+            this.Controls.Add(this.Dgv_registros);
+            this.Name = "Consulta";
+            this.Text = "Consulta";
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_registros)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Dgv_registros;
+        private System.Windows.Forms.Button Btn_Consultar;
+    }
+}
---
+++ Proyecto3/CapaVista/Consulta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaControlador;
+
+namespace CapaVista
+{
+    public partial class Consulta : Form
+    {
+        string emp = "empleados";
+        Controlador cn = new Controlador();
+        public Consulta()
+        {
+            InitializeComponent();
+        }
+
+
+        //Mostrar los datos CAPA VISTA
+        public void actualizardatagriew()
+        {
+            DataTable dt = cn.llenarTbl(emp);
+            Dgv_registros.DataSource = dt;
+
+        }
+
+        private void Btn_Consultar_Click(object sender, EventArgs e)
+        {
+            actualizardatagriew();
+        }
+    }
+}
